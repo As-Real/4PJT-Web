d03f47a32a9ad92c09d408cf4f3b57dea7e5cf60
Nino SACCOMANI <carkaneret@gmail.com>
2018-06-02 14:56:12 +0200

    Improve code & views readability

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -1,24 +1,31 @@
 var app = angular.module('app');
 app.controller('mainController', function ($scope, $rootScope, $location, $http, $cookies) {
-	$scope.root = {children: {}};
+
+    // $scope.root stores all data from user files/folder, recursively with a .children attribute for every folder
+    // When a button is clicked from a file/folder's menu (individual menu for each file/folder),
+    // with a ng-click, a reference to its location in $scope.root is given as an argument to the function
+    $scope.root = {children: {}};
 
 	//Can be null, "move" or "create"
 	$scope.mode = null;
 
 	$scope.toMove = null;
 
-	$scope.reactOnClick = function (object) {
-		if (object.type === "folder") {
-			if (!object.children) {
-				$scope.getFolderContent(object.path + "/", object);
-			}
-			else {
-				object.children = null;
-			}
-		}
-	};
+	//region LIST & DISPLAY
 
-	$scope.getFolderContent = function (path, container) {
+    $scope.reactOnClick = function (object) {
+        if (object.type === "folder") {
+            if (!object.children) {
+                $scope.getFolderContent(object.path + "/", object);
+            }
+            else {
+                object.children = null;
+            }
+        }
+    };
+
+
+    $scope.getFolderContent = function (path, container) {
 		$http.get('/api/files/list?path=' + path,
 			{
 				headers: {'Authorization': $cookies.get('auth')}
@@ -32,7 +39,9 @@ app.controller('mainController', function ($scope, $rootScope, $location, $http,
 				});
 	};
 	$scope.getFolderContent('/', $scope.root);
+	//endregion
 
+	//region DOWNLOAD
 
 	$scope.startDownload = function (object) {
 		var apiPath = object.type === "folder" ? "/api/folders/download" : "/api/files/download";
@@ -67,63 +76,9 @@ app.controller('mainController', function ($scope, $rootScope, $location, $http,
 		return fileName.replace(/"/g, '');
 	}
 
-	$scope.fileLoaded = function (ele) {
-		$scope.theFile = ele.files[0];
-	};
+	//endregion
 
-	$scope.startUpload = function (object) {
-		if (!$scope.theFile) {
-			return
-		}
-		//That phase of FormData is needed to send a file to the API
-		var fd = new FormData();
-		fd.append('incoming', $scope.theFile);
-		fd.append('path', object.path + "/");
-		//Call API
-		$http.post('/api/files/upload', fd, {
-			transformRequest: angular.identity,
-			headers: {
-				'Content-Type': undefined,
-				'Authorization': $cookies.get('auth')
-			}
-		})
-			.then(function (response) {
-				$scope.getFolderContent(object.path + "/", object)
-				$scope.showSnackBar("Fichier upload")
-
-			})
-			.catch(function (error) {
-				$scope.showSnackBar("Une erreur est survenue : " + error.data)
-			});
-	}
-
-
-	$scope.startRenaming = function (object) {
-		//Call API
-		var newPath;
-		var n = object.path.lastIndexOf(object.name);
-		if (n >= 0 && n + object.name.length >= object.path.length) {
-			newPath = object.path.substring(0, n) + object.newName;
-		} else {
-			return;
-		}
-
-		$http.post('/api/files/rename', {path: object.path, newPath: newPath},
-			{
-				headers: {'Authorization': $cookies.get('auth')}
-			})
-			.then(function (response) {
-					object.renaming = false;
-					object.name = object.newName;
-					object.path = newPath;
-					$scope.showSnackBar("Element renommé")
-
-				},
-				function (error) {
-					$scope.showSnackBar("Une erreur est survenue : " + error.data)
-				});
-
-	};
+	//region DELETE
 
 	$scope.startDelete = function (object) {
 		//Call API
@@ -146,22 +101,73 @@ app.controller('mainController', function ($scope, $rootScope, $location, $http,
 				});
 
 	};
+	//endregion
+
+    //region RENAME
+
+    //When ths user clicks on "Rename" button, $scope.renaming boolean is set to true
+    // ,so the name of the file/folder in the view is replaced by an input form
+    // A var $scope.newName is created. This var will hold the new name the folder/file should have after renaming
+    // ,$scope.newName is bound to the input with a ng-model
+    // The button "Rename" is replaced by a "Validate" button. When clicked, name is updated to newName in the storage
+
+    $scope.startRenaming = function (object) {
+        //Call API
+        var newPath;
+        var n = object.path.lastIndexOf(object.name);
+        if (n >= 0 && n + object.name.length >= object.path.length) {
+            newPath = object.path.substring(0, n) + object.newName;
+        } else {
+            return;
+        }
+
+        $http.post('/api/files/rename', {path: object.path, newPath: newPath},
+            {
+                headers: {'Authorization': $cookies.get('auth')}
+            })
+            .then(function (response) {
+                    object.renaming = false;
+                    object.name = object.newName;
+                    object.path = newPath;
+                    $scope.showSnackBar("Element renommé")
+
+                },
+                function (error) {
+                    $scope.showSnackBar("Une erreur est survenue : " + error.data)
+                });
+
+    };
+
+    //endregion
+
+    //region MOVE,ADD,UPLOAD
+
+	//When one of the buttons Move (on a file/folder); Create Folder (top menu) or Upload File (top menu) is clicked
+	//, the $scope.mode is set to a value.
+	//When this $scope.mode is different from null, folder icons are replaced by an red arrow.
+	//The user can then click on one of these arrows to choose this folder as the destination
+	//(move the file to this folder, create a folder in this folder or upload a file to this folder)
+	//The action triggered depends on the $scope.mode
+	//It is then set to null again
+
+	//When Move is clicked, the file to move is set in $scope.toMove
+
+    $scope.chooseFolder = function (folderParent) {
+        if ($scope.mode === "move") {
+            $scope.move(folderParent)
+        }
+        if ($scope.mode === "createFolder") {
+            $scope.createFolder(folderParent)
+        }
+        if ($scope.mode === "uploadFile") {
+            $scope.uploadFile(folderParent)
+        }
+    };
 
 
 	$scope.startMove = function (file) {
 		$scope.mode = 'move';
 		$scope.toMove = file;
-	};
-	$scope.chooseFolder = function (folderParent) {
-		if ($scope.mode === "uploadFile") {
-			$scope.uploadFile(folderParent)
-		}
-		if ($scope.mode === "createFolder") {
-			$scope.createFolder(folderParent)
-		}
-		if ($scope.mode === "move") {
-			$scope.move(folderParent)
-		}
 	};
 	$scope.move = function (parent) {
 		if (!$scope.toMove) {
@@ -173,7 +179,7 @@ app.controller('mainController', function ($scope, $rootScope, $location, $http,
 				headers: {'Authorization': $cookies.get('auth')}
 			})
 			.then(function (response) {
-					$scope.getFolderContent(parent.path + "/", parent)
+					$scope.getFolderContent(parent.path + "/", parent);
 					$scope.mode = null;
 					$scope.toMove.deleted = true;
 					if ($scope.toMove.type === "folder") {
@@ -185,31 +191,63 @@ app.controller('mainController', function ($scope, $rootScope, $location, $http,
 				function (error) {
 					$scope.showSnackBar("Une erreur est survenue : " + error.data)
 				});
-
 	};
-	$scope.uploadFile = function (parent) {
+
+    $scope.createFolder = function (parent) {
+        var folderName = prompt("Nom du dossier");
+        if (folderName) {
+            $http.post('/api/folders/add', {path: parent.path + '/', folderName: folderName},
+                {
+                    headers: {'Authorization': $cookies.get('auth')}
+                })
+                .then(function (response) {
+                        $scope.getFolderContent(parent.path + "/", parent)
+                        $scope.mode = null;
+                        $scope.showSnackBar("Dossier crée")
+                    },
+                    function (error) {
+                        $scope.showSnackBar("Une erreur est survenue : " + error.data)
+                    });
+        }
+    };
+
+
+    //FileLoaded is called when the user chooses a file to upload from its computer
+	//It keeps that file in memory, as itwill be uploaded in the storage
+    $scope.fileLoaded = function (ele) {
+        $scope.theFile = ele.files[0];
+    };
+
+    $scope.uploadFile = function (parent) {
 		$scope.startUpload(parent);
 		$scope.mode = null;
-
 	};
-	$scope.createFolder = function (parent) {
-		var folderName = prompt("Nom du dossier");
-		if (folderName) {
-			$http.post('/api/folders/add', {path: parent.path + '/', folderName: folderName},
-				{
-					headers: {'Authorization': $cookies.get('auth')}
-				})
-				.then(function (response) {
-						$scope.getFolderContent(parent.path + "/", parent)
-						$scope.mode = null;
-						$scope.showSnackBar("Dossier crée")
-					},
-					function (error) {
-						$scope.showSnackBar("Une erreur est survenue : " + error.data)
-					});
-		}
-	};
+    $scope.startUpload = function (object) {
+        if (!$scope.theFile) {
+            return
+        }
+        //That phase of FormData is needed to send a file to the API
+        var fd = new FormData();
+        fd.append('incoming', $scope.theFile);
+        fd.append('path', object.path + "/");
+        //Call API
+        $http.post('/api/files/upload', fd, {
+            transformRequest: angular.identity,
+            headers: {
+                'Content-Type': undefined,
+                'Authorization': $cookies.get('auth')
+            }
+        })
+            .then(function (response) {
+                $scope.getFolderContent(object.path + "/", object);
+                $scope.showSnackBar("Fichier upload")
 
+            })
+            .catch(function (error) {
+                $scope.showSnackBar("Une erreur est survenue : " + error.data)
+            });
+    };
+    //endregion
 
 	$scope.showSnackBar = function (message) {
 		var snackbarContainer = document.querySelector('#alert-snackbar');
